d4360382f6858d17e82d5bd5d2841b5eb670f80c
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-06 16:39:08 -0400

    HOMEWORK

---
+++ homework/writeloop.js
@@ -0,0 +1,44 @@
+// write a loop that out puts
+// 10
+// 9
+// 8
+// 7
+// 6
+// 5   
+// 4
+// 3
+// 2
+// 1
+// BLAST OFF!!!!!!!
+
+
+//let countDown = [
+//  10,
+//  9,
+//  8,
+ //  7,
+ //   6,
+ //   5,
+ //   4,
+ //   3,
+ //   2,
+ //   1,
+//]
+
+//console.log(`${countDown} BLAST OFF!!!!`)
+
+function countDown(num2) {
+    for (let i = num2; i >= 1; i--) {
+    console.log(i) ; {
+        if (i === 1)  
+        console.log('BLAST OFF!!!!🤯')
+    }
+}
+
+
+}
+
+
+countDown(12)
+
+//console.log('BLAST OFF')
